08a20fc2781ccd8f37c71dfd70dfe47ff18fef2e
Felix Volny <felix@volny.co>
2018-03-05 10:41:29 +0700

    eslint config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
-import Draggable, {DraggableCore} from 'react-draggable'; // Both at the same time
+import React, { Component } from 'react'
+import Draggable from 'react-draggable'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Draggable>
-          <div style={{
-            background: 'lightblue',
-            width: 200,
-            height: 200,
-            display: 'flex',
-            alignItems: 'center',
-            textAlign: 'center',
-          }}>I can now be moved around!</div>
-      </Draggable>
-    </div>
-    );
+        <Draggable axis="x">
+          <div
+            style={{
+              background: 'lightblue',
+              width: 400,
+              height: 40,
+              display: 'flex',
+              alignItems: 'center',
+              textAlign: 'center'
+            }}
+          >
+            I can now be moved around!
+          </div>
+        </Draggable>
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
