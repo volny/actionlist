0723cead14b37abbca22b73de4396014eeb8215d
Felix Volny <felix@volny.co>
2018-03-05 10:48:09 +0700

    sample rows

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Draggable axis="x">
-          <div
-            style={{
-              background: 'lightblue',
-              width: 400,
-              height: 40,
-              display: 'flex',
-              alignItems: 'center',
-              textAlign: 'center'
-            }}
-          >
-            I can now be moved around!
-          </div>
-        </Draggable>
+        {Array.from(Array(20), (_, index) =>
+          <Draggable axis="x">
+            <div
+              key={index}
+              style={{
+                background: 'lightsalmon',
+                width: 400,
+                height: 40,
+                display: 'flex',
+                alignItems: 'center',
+                textAlign: 'center'
+              }}
+            >
+              {`I am item #${index + 1}`}
+            </div>
+          </Draggable>
+      )}
       </div>
     )
   }
